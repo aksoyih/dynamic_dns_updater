f445bdd1fc40de1264d776192db40c8a28c2f8ac
İnanç Haluk Aksoy <54416061+aksoyih@users.noreply.github.com>
2024-02-19 23:17:38 +0300

    Update env variables

--- updater.sh
+++ updater.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Read environment variables
-mydomain="$MY_DOMAIN"
-myhostname="$MY_HOSTNAME"
+domain="$DNS_DOMAIN"
+hostname="$HOSTNAME"
 gdapikey="$GD_API_KEY"
 cfemail="$CF_EMAIL"
 cfapikey="$CF_API_KEY"
-logdest="$LOG_DEST"
+logdest="dns_update.log"
 
 # Ensure required environment variables are set
-if [[ -z "$mydomain" || -z "$myhostname" || -z "$gdapikey" || -z "$cfemail" || -z "$cfapikey" || -z "$logdest" ]]; then
+if [[ -z "$domain" || -z "$hostname" || -z "$gdapikey" || -z "$cfemail" || -z "$cfapikey" || -z "$logdest" ]]; then
   echo "Error: Environment variables not set properly."
   exit 1
 fi
@@ -18,11 +18,11 @@ fi
 myip=$(curl -s "https://api.ipify.org")
 
 # Fetch current DNS IP address from GoDaddy
-dnsdata=$(curl -s -X GET -H "Authorization: sso-key $gdapikey" "https://api.godaddy.com/v1/domains/$mydomain/records/A/$myhostname")
+dnsdata=$(curl -s -X GET -H "Authorization: sso-key $gdapikey" "https://api.godaddy.com/v1/domains/$domain/records/A/$hostname")
 gdip=$(echo "$dnsdata" | jq -r '.[].data')
 
 # Fetch current DNS IP address from Cloudflare
-cfip=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$mydomain" -H "X-Auth-Email: $cfemail" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" | jq -r '.result[0].id' | xargs -I {} curl -s -X GET "https://api.cloudflare.com/client/v4/zones/{}/dns_records?type=A&name=$myhostname.$mydomain" -H "X-Auth-Email: $cfemail" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" | jq -r '.result[0].content')
+cfip=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$domain" -H "X-Auth-Email: $cfemail" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" | jq -r '.result[0].id' | xargs -I {} curl -s -X GET "https://api.cloudflare.com/client/v4/zones/{}/dns_records?type=A&name=$hostname.$domain" -H "X-Auth-Email: $cfemail" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" | jq -r '.result[0].content')
 
 # Log current and GoDaddy DNS IP addresses
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Current External IP is $myip, GoDaddy DNS IP is $gdip, Cloudflare DNS IP is $cfip"
@@ -30,15 +30,15 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Current External IP is $myip, GoDaddy DNS I
 # Compare IP addresses and update GoDaddy DNS if necessary
 if [ "$gdip" != "$myip" ] && [ -n "$myip" ]; then
   echo "IP has changed!! Updating on GoDaddy"
-  curl -s -X PUT "https://api.godaddy.com/v1/domains/$mydomain/records/A/$myhostname" -H "Authorization: sso-key $gdapikey" -H "Content-Type: application/json" -d "[{\"data\": \"$myip\"}]"
-  logger -p "$logdest" "Changed IP on $myhostname.$mydomain from $gdip to $myip"
+  curl -s -X PUT "https://api.godaddy.com/v1/domains/$domain/records/A/$hostname" -H "Authorization: sso-key $gdapikey" -H "Content-Type: application/json" -d "[{\"data\": \"$myip\"}]"
+  logger -p "$logdest" "Changed IP on $hostname.$domain from $gdip to $myip"
 fi
 
 # Compare IP addresses and update Cloudflare DNS if necessary
 if [ "$cfip" != "$myip" ] && [ -n "$myip" ]; then
   echo "IP has changed!! Updating on Cloudflare"
-  zone_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$mydomain" -H "X-Auth-Email: $cfemail" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" | jq -r '.result[0].id')
-  record_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records?type=A&name=$myhostname.$mydomain" -H "X-Auth-Email: $cfemail" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" | jq -r '.result[0].id')
-  curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$record_id" -H "X-Auth-Email: $cfemail" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" --data "{\"type\":\"A\",\"name\":\"$myhostname.$mydomain\",\"content\":\"$myip\",\"ttl\":120,\"proxied\":false}"
-  logger -p "$logdest" "Changed IP on $myhostname.$mydomain from $cfip to $myip"
+  zone_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$domain" -H "X-Auth-Email: $cfemail" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" | jq -r '.result[0].id')
+  record_id=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records?type=A&name=$hostname.$domain" -H "X-Auth-Email: $cfemail" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" | jq -r '.result[0].id')
+  curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$record_id" -H "X-Auth-Email: $cfemail" -H "X-Auth-Key: $cfapikey" -H "Content-Type: application/json" --data "{\"type\":\"A\",\"name\":\"$hostname.$domain\",\"content\":\"$myip\",\"ttl\":120,\"proxied\":false}"
+  logger -p "$logdest" "Changed IP on $hostname.$domain from $cfip to $myip"
 fi
